c4ac4e212c02c4c258aec9389ae21fc2cb23a004
AntoineGS <antoineg.simard@gmail.com>
2016-04-06 21:08:54 -0400

    added format_date and edited calls in classes.

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,8 +3,14 @@ module Listable
     description.to_s.ljust(30)
   end
 
-  def format_date(date)
-
+  def format_date(options = {})
+    if options[:start_date] & options[:end_date]
+      dates = start_date.strftime('%D') if start_date
+      dates << ' -- ' + end_date.strftime('%D') if end_date
+      dates = 'N/A' if !dates
+    elsif options[:due]
+        due ? due.strftime('%D') : 'No due date'
+    end
   end
 
   def format_priority(priority)
